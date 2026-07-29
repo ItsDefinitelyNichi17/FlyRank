540aa822334ceea1089d666ed5b93d89fad876d0
chiniu <kimpoychin131@gmail.com>
2026-07-29 23:38:31 +0800

    Stage 1 : root and health endpoints

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.json({ name: "Task API", version: "1.0", enpoints: ["/tasks"] })
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: "ok" })
+})
+
 app.listen(3000, () => {
   console.log("Hello World, Server is at 3000")
 })
