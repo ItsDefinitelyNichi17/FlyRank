906d0e71da8dcb7fd271abb1a6440c055c5522bc
chiniu <kimpoychin131@gmail.com>
2026-07-30 00:20:05 +0800

    Stage 2 : read endpoints with 404

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,40 @@
 import express from 'express';
+
+const task = [
+  { id: 0, title: "Fly", done: true },
+  { id: 1, title: "Rank", done: true },
+  { id: 2, title: "AI", done: true }
+]
+
 const app = express();
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ name: "Task API", version: "1.0", enpoints: ["/tasks"] })
+  return;
 });
 
 app.get('/health', (req, res) => {
-  res.json({ status: "ok" })
+  res.status(200).json({ status: "ok" })
+  return;
+})
+
+app.get('/tasks/:id', (req, res) => {
+  const { id } = req.params
+
+  const foundId = task.filter((elem) => {
+    return elem.id === parseInt(id);
+  })
+
+  if (foundId.length === 0) {
+    res.status(404).json(`Error Task ${id} not found`);
+    return;
+  }
+
+  res.status(200).json({ task : foundId[0] });
+  return
+
 })
 
 app.listen(3000, () => {
