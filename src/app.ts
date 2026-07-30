5607b949e2a0df73dc09431d87040c2e753df4f2
chiniu <kimpoychin131@gmail.com>
2026-07-30 11:14:27 +0800

    Stage 3 : create with validation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 
 const task = [
-  { id: 0, title: "Fly", done: true },
-  { id: 1, title: "Rank", done: true },
-  { id: 2, title: "AI", done: true }
+  { id: 1, title: "Walk Dawg", done: true },
+  { id: 2, title: "Cook Meal", done: true },
+  { id: 3, title: "Study", done: true }
 ]
 
 const app = express();
@@ -35,6 +35,36 @@ app.get('/tasks/:id', (req, res) => {
   res.status(200).json({ task : foundId[0] });
   return
 
+})
+
+app.post('/tasks', (req, res) => {
+  const { title } = req.body;
+
+  try {
+    if (!title || !title.trim()) {
+      res.status(400).json({ error: "Bad Request : Please define a title" });
+      return;
+    }
+
+    let newObj = {
+      id: task.length - 1,
+      title: title,
+      done: false
+    }
+
+    task.push(newObj);
+    res.status(201).json({ newtask: newObj })
+
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({ error: "Internal Server Error" })
+  }
+  return;
+})
+
+app.get('/tasks', (req, res) => {
+  res.status(200).json({ tasks: task });
+  return;
 })
 
 app.listen(3000, () => {
