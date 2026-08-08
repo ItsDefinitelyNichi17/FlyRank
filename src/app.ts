02a14708137fd1172795ff4364821c88a1b9a5f1
nichi <kimpoychin131@gmail.com>
2026-08-08 07:30:27 +0000

    Stage 3: update and delete with SQL

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
-import { getTask, getTaskWithID, postTask } from './controllers/task.controller.js';
+import { deleteTask, getTask, getTaskWithID, postTask, updateTask} from './controllers/task.controller.js';
+
 let task = [
   { id: 1, title: "Walk Dawg", done: true },
   { id: 2, title: "Cook Meal", done: false },
@@ -28,55 +29,15 @@ app.get('/health', (req, res) => {
   return;
 });
 
-app.get('/tasks/:id', getTaskWithID)
+app.get('/tasks/:id', getTaskWithID);
 
 app.get('/tasks', getTask);
 
-
 app.post('/tasks', postTask);
 
-app.put('/tasks/:id', (req, res) => {
-  const { id } = req.params;
-  const { title, done } = req.body;
+app.put('/tasks/:id', updateTask);
 
-  let findId = task.findIndex((e) => {
-    return (e.id === parseInt(id))
-  });
-
-  if (title) {
-    if (!title.trim()) {
-      res.status(400).json({ message: "Bad Request: Invalid Body" })
-      return;
-    }
-  }
-
-  if (findId === -1) {
-    res.status(404).json({ message: "Not found" })
-    return;
-  }
-  task[findId] = {
-    id: parseInt(id),
-    title: title ?? task[findId]!.title,
-    done: done ?? task[findId]!.done
-  };
-
-  res.status(200).json({ updatedTask : task[findId] });
-})
-
-app.delete('/tasks/:id', (req, res) => {
-
-  const { id } = req.params;
-
-  const indexToRemove = task.findIndex((e) => e.id === parseInt(id));
-
-  if (indexToRemove === -1) {
-    res.status(404).json({ message: "Not found" })
-    return;
-  }
-  const removedItem = task.splice(indexToRemove, 1);
-  res.status(204).send();
-  return;
-})
+app.delete('/tasks/:id', deleteTask)
 
 app.get('/stats', (req, res) => {
   const totalTasks = task.length;
@@ -85,6 +46,4 @@ app.get('/stats', (req, res) => {
   res.status(200).json({ total: totalTasks, done: doneTasks, open: openTasks });
 })
 
-app.listen(3000, () => {
-  console.log("Hello World, Server is at 3000")
-})
+app.listen(3000, () => {  console.log("Hello World, Server is at 3000");});
--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -5,7 +5,6 @@ import db from "../db.js";
 
 export function getTask(req: Request, res: Response) {
   const { done, title } = req.query;
-
   const query = db.prepare("SELECT * FROM tasks").all() as Array<{id : number, title : string, done : boolean}>;
 
   const result = query.map((e) => {
@@ -21,17 +20,22 @@ export function getTask(req: Request, res: Response) {
 
 export function getTaskWithID(req: Request, res: Response) {
   const { id } = req.params
-  let result = db.prepare(`SELECT * FROM tasks WHERE id = ?`).get(id) as {id : number, title : string, done : boolean}
-  result = { ...result, done : result.done ? true: false }
+  const findId = db.prepare("SELECT * FROM tasks WHERE id = ?").get(parseInt(id as string))
 
-  if (!result) {
-    res.status(404).json({message: "Not found"})
+  if (!findId) {
+    res.status(404).json({ message: "Not found" })
     return;
   }
-
-  res.status(200).json({result : result})
+  try {
+    let result = db.prepare(`SELECT * FROM tasks WHERE id = ?`).get(id) as {id : number, title : string, done : boolean}
+    result = { ...result, done: result.done ? true : false }
+    res.status(200).json({ result: result })
+    return;
+  } catch (e) {
+    res.status(500)
+    return;
+  }
 }
-
 
 export function postTask(req: Request, res: Response) {
   const { title } = req.body
@@ -40,11 +44,53 @@ export function postTask(req: Request, res: Response) {
       res.status(400).json({ message: 'Cannot process the request, title should be define' });
       return;
     }
-    const task = db.prepare('INSERT INTO tasks(title) VALUES (?)').run(title);
+    const task = db.prepare('INSERT INTO tasks(title) VALUES (?) RETURNING *').get(title);
 
     res.status(201).json({task : task})
     return
   } catch (e) {
-    console.log(e)
+    res.status(500).send()
   }
+}
+
+export function updateTask(req: Request, res: Response) {
+  let { id } = req.params;
+  let { title, done} = req.body;
+
+  const findId = db.prepare("SELECT * FROM tasks WHERE id = ?").get(parseInt(id as string)) as {id : number, title : string, done : number}
+
+  if (!findId) {
+    res.status(404).json({ message: "Could not find the id" })
+    return;
+  }
+
+  if (title || title.length === 0){
+    if (!title.trim()) {
+      res.status(400).json({ message: "Bad Request: Invalid Body" })
+      return;
+    }
+  }
+
+  done = done ? 1 : 0;
+  title = title ? title : findId.title
+
+  let updaterec = db.prepare("UPDATE tasks SET title = ?, done = ? WHERE id = ? RETURNING *;")
+    .get(title, done, parseInt(id as string)) as { id: number, title: string, done: boolean };
+
+  updaterec = { ...updaterec, done: updaterec.done ? true : false }
+  res.status(200).json({task : updaterec});
+  return;
+}
+
+export function deleteTask(req: Request, res: Response) {
+  const { id } = req.params;
+  const findId = db.prepare("SELECT * FROM tasks WHERE id = ?").get(parseInt(id as string))
+
+  if (!findId) {
+    res.status(404).json({ message: "Could not find the id" })
+    return;
+  }
+
+  const del = db.prepare("DELETE FROM tasks WHERE id = ?").run(id);
+  res.status(204).send();
 }
